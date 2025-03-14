315e87f19a4f48c3b0bb518f3bc6b5a51c3f82c9
MateoSebF <mateo.forero-f@mail.escuelaing.edu.co>
2025-03-14 00:52:00 -0500

    change cors

--- src/main/java/co/edu/eci/arep/springweb/config/SecurityConfig.java
+++ src/main/java/co/edu/eci/arep/springweb/config/SecurityConfig.java
@@ -2,15 +2,31 @@ package co.edu.eci.arep.springweb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .cors(Customizer.withDefaults()) // Habilita CORS
+            .csrf(csrf -> csrf.disable()) // Desactiva CSRF si no usas formularios en Spring
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll() // Permite todas las solicitudes sin autenticación
+            );
+        return http.build();
     }
     
 }
